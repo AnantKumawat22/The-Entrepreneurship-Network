022fb28162d2df762a263ed8fd2f6367121a3f1a
Anant Kumawat <anantkumawat22@gmail.com>
2021-08-30 19:25:50 +0530

    third commit

--- src/Home/Slider.jsx
+++ src/Home/Slider.jsx
@@ -8,7 +8,7 @@ export default styled.div`
   width: 100%;
   background: url("https://static.wixstatic.com/media/11062b_558f00357585424e894fb7c595fc38fd~mv2.jpg/v1/fill/w_1520,h_620,al_c,q_85,usm_0.66_1.00_0.01/11062b_558f00357585424e894fb7c595fc38fd~mv2.webp");
   color: #fff;
-  margin: 0px 5px;
+  margin: 0px -10px;
   font-size: 27px;
   text-align: center;
   font-family: 'noticia text', serif;
